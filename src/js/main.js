c6f9b2a870decbd87f779115a8fc5cd9ba241ffd
tranzet23 <vladislavzyuzyukov@gmail.com>
2022-06-07 12:29:10 +0300

    add build to gitignore

--- src/js/main.js
+++ src/js/main.js
@@ -22,7 +22,6 @@ accordion('.accordion__item', 'accordion__item--active');
 
 
 // slider-banner
-
 $('.slider-banner__items').slick({
 	infinite: true,
 	slidesToShow: 1,
@@ -34,14 +33,13 @@ $('.slider-banner__items').slick({
   });
 
 // slider-card
-
 $('.card-slider__inner').slick({
 	slidesToShow: 1,
 	arrows: false,
 	dots: true,
 });
-  // slider-brand
 
+  // slider-brand
 $('.brands__items-slider').slick({
 	infinite: true,
 	slidesToShow: 7,
@@ -174,3 +172,53 @@ $('.tabs__item').click(function () {
 	$('.tabs__item').removeClass('tabs__item-active').eq($(this).index()).addClass('tabs__item-active');
 	$('.tabs__content').hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass('tabs__item-active');
+
+
+
+
+
+
+
+
+
+
+try{
+	$(".custom-select").each(function() {
+		var classes = $(this).attr("class"),
+			id      = $(this).attr("id"),
+			name    = $(this).attr("name");
+		var template =  '<div class="' + classes + '">';
+		template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + '</span>';
+		template += '<div class="custom-options">';
+		$(this).find("option").each(function() {
+			template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+		});
+		template += '</div></div>';
+
+		$(this).wrap('<div class="custom-select-wrapper"></div>');
+		$(this).hide();
+		$(this).after(template);
+	});
+	$(".custom-option:first-of-type").hover(function() {
+		$(this).parents(".custom-options").addClass("option-hover");
+	}, function() {
+		$(this).parents(".custom-options").removeClass("option-hover");
+	});
+	$(".custom-select-trigger").on("click", function() {
+		$('html').one('click',function() {
+			$(".custom-select").removeClass("opened");
+		});
+		$(this).parents(".custom-select").toggleClass("opened");
+		event.stopPropagation();
+	});
+	$(".custom-option").on("click", function() {
+		$(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
+		$(this).parents(".custom-options").find(".custom-option").removeClass("selection");
+		$(this).addClass("selection");
+		$(this).parents(".custom-select").removeClass("opened");
+		$(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
+	});
+}
+catch {
+	
+}
